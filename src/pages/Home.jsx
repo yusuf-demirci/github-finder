b2a4352fde64a3d4be75fab70dc37aae85df1adc
Yusuf Demirci <ydemirci7@gmail.com>
2022-02-28 23:25:40 +0300

    search feature added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
 
 function Home() {
   return (
-    <div>
-        <h1 className="text-6xl">Welcome</h1>
-    </div>
+    <>
+        <UserSearch />
+        <UserResults />
+    </>
   )
 }
 
